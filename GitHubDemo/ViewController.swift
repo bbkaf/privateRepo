b78b426f1c3aac66ca11443063af243b948d004c
bbkaf <30632940+bbkaf@users.noreply.github.com>
2017-08-02 01:29:50 +0800

    add jessy by remoteRepo

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let user = "Hank"
+    let user = "Hank jessy"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
